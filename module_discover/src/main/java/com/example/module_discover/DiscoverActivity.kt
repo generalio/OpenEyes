77b82e56c5b93692ace0b07d6df2819d229103ae
黎枝ღ <3291449076@qq.com>
2025-07-13 17:40:42 +0800

    完成发现页面的整体ui设计

---
+++ module_discover/src/main/java/com/example/module_discover/DiscoverActivity.kt
@@ -0,0 +1,12 @@
+package com.example.module_discover
+
+import android.os.Bundle
+import com.generals.lib.base.BaseActivity
+
+class DiscoverActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_discover)
+
+    }
+}
